31500f32385e4c81ed03002fe01669f5a6ac276f
Akash <aakash23kriplani@gmail.com>
2022-01-25 12:14:42 +0530

    Add trigger method

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,16 @@ import { User } from "./models/User";
 
 const user = new User({ name: "John", age: 36 });
 
-user.set({name: "Jack"});
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.on("change", () => {
+  console.log('Change #1');
+});
+
+user.on("change", () => {
+  console.log('Change #2');
+});
+
+user.on("save", () => {
+  console.log('Save was triggered');
+});
+
+user.trigger('save');
