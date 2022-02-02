1ade358ab7dc4094f670d7469fe1c32f631bdcab
Akash <aakash23kriplani@gmail.com>
2022-02-02 11:23:14 +0530

    Fetch user from the database

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,4 @@
+import { AxiosResponse } from 'axios';
 import { Attributes } from './Attributes';
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
@@ -32,8 +33,22 @@ export class User {
     return this.attributes.get;
   }
 
-  set(update: UserProps) {
+  set(update: UserProps): void {
     this.attributes.set(update);
     this.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.get('id');
+
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without an id');
+    }
+
+    this.sync.fetch(id).then(
+      (response: AxiosResponse): void => {
+        this.set(response.data);
+      }
+    );
   }
 }
